de167869fcdf0d0da0aee034f5a677ae0ef967f6
YuriyTimoshenkov <xSamadhix@gmail.com>
2015-03-01 23:24:55 +0200

    Update NPC.cs

--- SlowpokeEngine/Bodies/NPC.cs
+++ SlowpokeEngine/Bodies/NPC.cs
@@ -6,7 +6,7 @@ namespace SlowpokeEngine.Bodies
 {
 	public class NPC : ActiveBody
 	{
-		private volatile Timer timer; //!!!Опасно комилятор в релизе вычленит timer из теля метода и перенесет в конструктор так как используется он только там 
+		private volatile Timer timer;
 		public NPC ():base(new Tuple<int,int>(0,0),new Tuple<int,int>(1,1))
 		{
 			timer = new Timer (Move, null, 0, 1000);
