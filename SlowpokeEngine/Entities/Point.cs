c4d13b64e31f28d2628686b4d992c2d0d82e1004
Aronax <aronax@zeoalliance.com>
2015-03-02 09:03:23 +0200

    Add Point.cs

---
+++ SlowpokeEngine/Entities/Point.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+namespace SlowpokeEngine.Entities
+{
+	[Serializable]
+	public struct Point
+	{
+		private  int _x;
+		private  int _y;
+
+		public Point(int x, int y)
+		{
+			_x = x;
+			_y = y;
+		}
+
+		[Browsable(false)]
+		public bool IsZero
+		{
+			get { return _x == 0 && _y == 0; }
+		}
+
+		public int X
+		{
+			get { return _x; }
+			set { _x = value; }
+		}
+
+		public int Y
+		{
+			get { return _y; }
+			set { _y = value; }
+		}
+
+		#region Static
+
+		public static readonly Point Empty = new Point();
+
+		public static bool operator ==(Point left, Point right)
+		{
+			return left.X == right.X && left.Y == right.Y;
+		}
+
+		public static bool operator !=(Point left, Point right)
+		{
+			return !(left == right);
+		}
+
+		public static Point operator +(Point pt, Point sz)
+		{
+			return Add(pt, sz);
+		}
+
+		public static Point Add(Point pt, Point sz)
+		{
+			return new Point(pt.X + sz.X, pt.Y + sz.Y);
+		} 
+
+		public static Point operator -(Point pt, Point sz)
+		{
+			return Subtract(pt, sz);
+		}
+
+		public static Point Subtract(Point pt, Point sz)
+		{
+			return new Point(pt.X - sz.X, pt.Y - sz.Y);
+		} 
+
+		public override bool Equals(object obj)
+		{
+			if (!(obj is Point)) return false;
+			var comp = (Point) obj;
+			return comp.X == X && comp.Y == Y;
+		}
+
+		public override int GetHashCode()
+		{
+			return _x ^ _y;
+		}
+
+		public void Offset(int dx, int dy)
+		{
+			X += dx;
+			Y += dy;
+		}
+
+		public void Offset(Point p)
+		{
+			Offset(p.X, p.Y);
+		}
+
+		public static implicit operator int[](Point p)
+		{
+			return new[] {p.X, p.Y};
+		}
+
+		public static implicit operator Point(int[] array)
+		{
+			if(array == null)
+				return new Point(0,0);
+
+			return array.Length < 2
+				? new Point(0, 0)
+				: new Point(array[0], array[1]);
+		}
+
+		public static implicit operator Point(Tuple<int, int> tuple)
+		{
+			return tuple == null
+				? new Point(0,0)
+				: new Point(tuple.Item1, tuple.Item2);
+		}
+
+		public static implicit operator Tuple<int, int>(Point p)
+		{
+			return new Tuple<int, int>(p.X, p.Y);
+		}
+
+
+		#endregion Static 
+
+		public override string ToString()
+		{
+			return "{X=" + X.ToString(CultureInfo.CurrentCulture) + ", Y=" + Y.ToString(CultureInfo.CurrentCulture) + "}";
+		}
+
+	}
+}
---
+++ SlowpokeSelfHost/TupleDecompiled.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlowpokeSelfHost
+{
+	[Serializable]
+	public class TupleDecompiled<T1, T2> : IStructuralEquatable, IStructuralComparable, IComparable, ITuple
+	{
+
+		private readonly T1 m_Item1;
+		private readonly T2 m_Item2;
+
+		public T1 Item1 { get { return m_Item1; } }
+		public T2 Item2 { get { return m_Item2; } }
+
+		
+		public TupleDecompiled(T1 item1, T2 item2)
+		{
+			m_Item1 = item1;
+			m_Item2 = item2;
+		}
+
+		public override Boolean Equals(Object obj)
+		{
+			return ((IStructuralEquatable)this).Equals(obj, EqualityComparer<Object>.Default); ;
+		}
+
+		Boolean IStructuralEquatable.Equals(Object other, IEqualityComparer comparer)
+		{
+			if (other == null) return false;
+
+			var objTuple = other as Tuple<T1, T2>;
+
+			if (objTuple == null)
+			{
+				return false;
+			}
+
+			return comparer.Equals(m_Item1, objTuple.Item1) && comparer.Equals(m_Item2, objTuple.Item2);
+		}
+
+		Int32 IComparable.CompareTo(Object obj)
+		{
+			return ((IStructuralComparable)this).CompareTo(obj, Comparer<Object>.Default);
+		}
+
+		Int32 IStructuralComparable.CompareTo(Object other, IComparer comparer)
+		{
+			if (other == null) return 1;
+
+			var objTuple = other as Tuple<T1, T2>;
+
+			if (objTuple == null)
+			{
+				throw new ArgumentException(Environment.GetResourceString("ArgumentException_TupleIncorrectType", this.GetType().ToString()), "other");
+			}
+
+			int c = 0;
+
+			c = comparer.Compare(m_Item1, objTuple.Item1);
+
+			if (c != 0) return c;
+
+			return comparer.Compare(m_Item2, objTuple.Item2);
+		}
+
+		public override int GetHashCode()
+		{
+			return ((IStructuralEquatable)this).GetHashCode(EqualityComparer<Object>.Default);
+		}
+
+		Int32 IStructuralEquatable.GetHashCode(IEqualityComparer comparer)
+		{
+			return Tuple.CombineHashCodes(comparer.GetHashCode(m_Item1), comparer.GetHashCode(m_Item2));
+		}
+
+		Int32 ITuple.GetHashCode(IEqualityComparer comparer)
+		{
+			return ((IStructuralEquatable)this).GetHashCode(comparer);
+		}
+		public override string ToString()
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.Append("(");
+			return ((ITuple)this).ToString(sb);
+		}
+
+		string ITuple.ToString(StringBuilder sb)
+		{
+			sb.Append(m_Item1);
+			sb.Append(", ");
+			sb.Append(m_Item2);
+			sb.Append(")");
+			return sb.ToString();
+		}
+
+		int ITuple.Size
+		{
+			get
+			{
+				return 2;
+			}
+		}
+	}
+}
