a5a8e0e11d103fc3fe431d884cd08841308bdcc3
Yuriy Timoshenkov <xsamadhix@gmail.com>
2015-03-10 16:59:32 +0200

    Game engine migration to Cabinet #2

---
+++ SlowpokeHostASPNET/App_Start/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using SlowpokeHubs;
+using System;
+
+
+namespace SlowpokeHostASPNET
+{
+    public partial class Startup
+    {
+        // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
+        public void Configuration(IAppBuilder app)
+        {
+            // Enable the application to use a cookie to store information for the signed in user
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                SlidingExpiration = false,
+                ExpireTimeSpan = new TimeSpan(0,1,0),//TODO use one config storage
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login")
+            });
+
+            app.MapSignalR();
+
+            SlowpokeHub.Run();
+        }
+    }
+}
