ee03a65958325feceff6e9f7f6194bdfaf45e3fd
Yuriy Timoshenkov <xsamadhix@gmail.com>
2015-03-25 15:07:33 +0200

    #17 Implement player games statistic storing on Cabinet
    Add forgotten GameCharacter file

---
+++ SlowpokeEngine/Entities/GameCharacter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlowpokeEngine
+{
+    public class GameCharacter
+    {
+        public Guid Id { get; set; }
+        public Guid OwnerUserId { get; set; }
+
+        public GameCharacter() { }
+
+        public GameCharacter(Guid onwerUserId)
+        {
+            Id = Guid.NewGuid();
+            OwnerUserId = onwerUserId;
+        }
+    }
+}
