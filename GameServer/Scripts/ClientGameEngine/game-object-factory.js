ee58480a24aedd81e1c9d7038de5ef0047f7bd7c
dimapct <dimapct@gmail.com>
2015-05-15 22:46:36 +0300

    Game Object Factories

---
+++ GameServer/Scripts/ClientGameEngine/game-object-factory.js
@@ -0,0 +1,37 @@
+﻿function GameObjectFactory() {
+    this.createGameObject = function (gameType, data) {
+        return this.builders[gameType](data);
+    }
+
+    this.builders = []
+    this.builders[gameTypes.gameObjects.PLAYER] = function (data) {
+        
+    }
+    this.builders[gameTypes.gameObjects.PLAYEROTHER] = function (data) {
+
+    }
+    this.builders[gameTypes.gameObjects.NPC] = function (data) {
+
+    }
+    this.builders[gameTypes.gameObjects.BULLET] = function (data) {
+
+    }
+    this.builders[gameTypes.gameObjects.LIFECONTAINER] = function (data) {
+
+    }
+    this.builders[gameTypes.gameObjects.REVOLVER] = function (data) {
+
+    }
+    this.builders[gameTypes.gameObjects.GUN] = function (data) {
+
+    }
+    this.builders[gameTypes.gameObjects.SHOTGUN] = function (data) {
+
+    }
+    this.builders[gameTypes.gameObjects.DYNAMITE] = function (data) {
+
+    }
+
+
+
+}
---
+++ GameServer/Scripts/ClientGameEngine/game-types.js
@@ -0,0 +1,14 @@
+﻿gameTypes = {
+    gameObjects: {
+        PLAYER: 0,
+        PLAYEROTHER: 1,
+        NPC: 2,
+        BULLET: 3,
+        LIFECONTAINER: 4,
+        REVOLVER: 5,
+        GUN: 6,
+        SHOTGUN: 7,
+        DYNAMITE: 8
+    }
+}
+
