bde0004b66a7be397075bbd49d987407bcdedd15
dimapct <dimapct@gmail.com>
2015-10-09 23:19:55 +0300

    To commit PhysicsEngine2

---
+++ GameServer/Scripts/Engine_TS/Engines/PhysicsEngine/physics-engine.ts
@@ -0,0 +1,20 @@
+﻿class PhysicsEngine implements IPhysicsEngine {
+
+    constructor(shapeCollisionManager: IShapeCollisionManager) {
+
+    }
+
+    processBodyAction(command: CommandBase): PhysicsProcessingResult {
+        //foreach(var handler in _commandHandlers)
+        //{
+        //    if (handler.Item1(command))
+        //        return handler.Item2(command);
+        //}
+        return new PhysicsProcessingResultEmpty();
+    }
+
+
+    toDelete() {//dfsf }
+
+    }
+}
